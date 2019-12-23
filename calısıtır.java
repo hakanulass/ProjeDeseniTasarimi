0691333bf86bc082dc8f4251e81b13d1b970bdf5
hakanulass <59179295+hakanulass@users.noreply.github.com>
2019-12-24 00:00:33 +0300

    Add files via upload

---
+++ calısıtır.java
@@ -0,0 +1,11 @@
+
+public class calıstır {
+
+	 public static void main( final String[] args ) {
+
+	      	  int sayi=10;
+	    	  sayimIslemi say = new sayimIslemi();
+		      urun sayi = say.sayimaralıgı(sayi);
+		      sayi.islem(sayi);
+	   }
+}
